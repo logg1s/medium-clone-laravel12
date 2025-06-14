d05824e3d3572436a0546bfb8f85c6ef2f08675c
logg1s <lrng159@gmail.com>
2025-06-14 10:30:26 +0700

    complete migration and seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->sentence();
+        return [
+            'image' => 'https://picsum.photos/640/480',
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'content' => fake()->paragraph(),
+            'category_id' => Category::inRandomOrder()->first()->id,
+            'user_id' => User::first()->id,
+            'published_at' => fake()->dateTime()
+        ];
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'long',
+            'email' => 'doremon1234567890@gmail.com',
+        ]);
+    }
+}
